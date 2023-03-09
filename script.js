ea9e0cb84413325546ac6181907b6ae5e3b98852
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-03-09 10:18:07 -0300

    smooth scrolling

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ document.addEventListener("keydown", function (e) {
 
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
+/* 
 console.log(document.documentElement)
 console.log(document.head)
 console.log(document.body)
@@ -44,8 +45,7 @@ const allButtons = document.getElementsByTagName("button")
 console.log(allButtons)
 
 console.log(document.getElementsByClassName("btn"))
-
-/* Creating and inserting elements */
+// Creating and inserting elements
 const message = document.createElement("div")
 message.classList.add("cookie-message")
 message.textContent = "We use cookies for improved functionality and analytics."
@@ -59,8 +59,80 @@ header.append(message)
 // header.before(message)
 header.after(message)
 
-/* Delete elements */
+// Delete elements
 document.querySelector(".btn--close-cookie").addEventListener("click", () => {
 	//message.parentElement.removeChild(message)
 	message.remove()
+})
+
+console.log("STYLES =======================================")
+// Styles
+message.style.backgroundColor = "#37383d"
+message.style.width = "120%"
+
+console.log(getComputedStyle(message).height)
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px"
+
+document.documentElement.style.setProperty("--color-primary", "orangered")
+
+console.log("ATTRIBUTES =======================================")
+
+// Attributes
+const logo = document.querySelector(".nav__logo")
+console.log(logo.alt)
+console.log(logo.src)
+console.log(logo.className)
+
+console.log(logo.nonStandardAttribute)
+
+console.log(logo.getAttribute("nonStandardAttribute"))
+logo.setAttribute("company", "Bankist")
+console.log(logo.src)
+console.log(logo.getAttribute("src"))
+
+const link = document.querySelector(".nav__link--btn")
+console.log(link.href)
+console.log(link.getAttribute("href"))
+
+// Data attributes
+console.log(logo.dataset.versionNumber)
+
+//Classes
+logo.classsList.add("c", "j") // set more than one class
+logo.classList.remove("c", "j")
+logo.classList.toggle("c")
+logo.classList.contains("c")
+
+// Don't use (overwrite other classes)
+location.className = "jonas"
+ */
+
+const btnScrollTo = document.querySelector(".btn--scroll-to")
+
+const section1 = document.querySelector("#section--1")
+
+btnScrollTo.addEventListener("click", function (e) {
+	const s1coords = section1.getBoundingClientRect()
+	console.log(s1coords)
+
+	console.log(e.target.getBoundingClientRect())
+
+	console.log("Current  scroll (X/Y)", window.pageXOffset, window.pageYOffset)
+	console.log(
+		"width/height",
+		document.documentElement.clientWidth,
+		document.documentElement.clientHeight
+	)
+
+	// Scrolling					x-coord				y-coord
+	// window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset)
+
+	/* window.scrollTo({
+		left: s1coords.left + window.pageXOffset,
+		top: s1coords.top + window.pageYOffset,
+		behavior: "smooth"
+	}) */
+
+	section1.scrollIntoView({ behavior: "smooth" })
 })
