a623bcc525544c1cc0f3c52e85381a0902c161ad
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-03-14 02:27:04 -0300

    implementing sticky navigation

--- script.js
+++ script.js
@@ -113,7 +113,6 @@ tabsContainer.addEventListener("click", function (e) {
 
 // Menu fade animation
 const handleHover = function (e) {
-	console.log(this, e.currentTarget)
 	if (e.target.classList.contains("nav__link")) {
 		const link = e.target
 		const siblings = link.closest(".nav").querySelectorAll(".nav__link")
@@ -130,6 +129,54 @@ const handleHover = function (e) {
 // Passing argument into handler
 nav.addEventListener("mouseover", handleHover.bind(0.5))
 nav.addEventListener("mouseout", handleHover.bind(1))
+
+// Sticky navigation (Scroll Event)
+// const initialCoords = section1.getBoundingClientRect()
+// console.log(initialCoords)
+
+// window.addEventListener("scroll", () => {
+// 	console.log(window.scrollY)
+
+// 	if (window.scrollY > initialCoords.top) {
+// 		nav.classList.add("sticky")
+// 	} else nav.classList.remove("sticky")
+// })
+
+// Sticky navigation (Intersection Observer API)
+/* const obsCallback = function (entries, observer) {
+	entries.forEach((entry) => {
+		console.log(entry)
+	})
+}
+
+const obsOptions = {
+	root: null,
+	threshold: 0.1
+}
+
+const observer = new IntersectionObserver(obsCallback, obsOptions)
+observer.observe(section1) */
+
+const header = document.querySelector(".header")
+const navHeight = nav.getBoundingClientRect().height
+
+const stickyNav = function (entries) {
+	const [entry] = entries
+	console.log(entry)
+	if (!entry.isIntersecting) {
+		nav.classList.add("sticky")
+	} else {
+		nav.classList.remove("sticky")
+	}
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+	root: null,
+	threshold: 0,
+	rootMargin: `-${navHeight}px`
+})
+headerObserver.observe(header)
+
 /* 
 console.log(document.documentElement)
 console.log(document.head)
